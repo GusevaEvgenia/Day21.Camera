35d068da66fd5adfecf47045166fc72007aef5d6
Belka <Evgenia_@email.ua>
2015-04-16 15:45:29 +0300

    add something dont finish lessons

--- app/src/main/java/com/android_lessons/belkin/day21camera/MainActivity.java
+++ app/src/main/java/com/android_lessons/belkin/day21camera/MainActivity.java
@@ -1,19 +1,45 @@
 package com.android_lessons.belkin.day21camera;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public final int CAMERA_RESULT = 0;
+    Button FlashLightControl;
+    ImageView ivCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FlashLightControl = (Button) findViewById(R.id.flashcontrol);
+        ivCamera = (ImageView) findViewById(R.id.iv_camera);
     }
 
+    // Щелчок кнопки
+    public void CameraTurnClick(View v) {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_RESULT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_RESULT) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            ivCamera.setImageBitmap(thumbnail);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
